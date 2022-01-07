d2d44165b05818373487f3f9ea7c726df2abaf8c
Artem <artem.tolpa@gmail.com>
2022-01-07 14:40:09 +0200

    Display all words from the vocabulary (database)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [words, setWords] = useState(null);
+
+  useEffect(() => {
+    axios("http://localhost:8080/words").then(({ data }) => {
+      setWords(data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Words</h2>
+      {words === null
+        ? "Loading"
+        : words.map((word) => (
+            <div key={word.id}>
+              {word.fin} = {word.eng}
+            </div>
+          ))}
     </div>
   );
 }
